3e58ad18fd647cc760a7d8c4d2cedd6096de4317
Nog <63429016+GonzaloNog@users.noreply.github.com>
2023-03-30 20:57:24 -0300

    Ejercicio

---
+++ Ejercicios/Clase2_1.py
@@ -0,0 +1,8 @@
+'''
+crea un loop, en el que una persona pueda ingresar palabras sueltas, tenes que comprobar que la palabra no tenga espacios
+osea, que no sea mas d euna palabra
+
+si la palabra es "quit" termina el loop
+si la palabra es 'imprimir' muestra todas las palabras ingresadas
+tenes que guardar todas las palabras y no pueden estar palabras repetidas
+'''
